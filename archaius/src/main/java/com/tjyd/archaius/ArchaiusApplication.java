0c83c2e4f82c074ef8f75350de91a0f695b8474e
zhao <26832684@qq.com>
2018-04-18 22:25:08 +0800

    Initial commit

---
+++ archaius/src/main/java/com/tjyd/archaius/ArchaiusApplication.java
@@ -0,0 +1,12 @@
+package com.tjyd.archaius;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchaiusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchaiusApplication.class, args);
+	}
+}
---
+++ ribbon/src/main/java/com/tjyd/ribbon/RibbonApplication.java
@@ -0,0 +1,12 @@
+package com.tjyd.ribbon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RibbonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RibbonApplication.class, args);
+    }
+}
